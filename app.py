4978ddbf8b1bf88486e823463c85a3ed3ce1427a
Suchitra Patil <58353545+suchitra2019-sudo@users.noreply.github.com>
2026-04-16 16:09:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -515,7 +515,7 @@ if page=="Create Invoice":
                 company, address, gst, logo_path,
                 invoice_no, invoice_date,
                 customer_name, contact, gstin,
-                items, subtotal, , transport, total
+                items, subtotal,gst_amount, transport, total
             )
 
             st.success("Invoice Created")
